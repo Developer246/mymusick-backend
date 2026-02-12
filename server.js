a38fc3765fd74f68a08bb84ae547f25abc532403
Developer246 <bobicraftyt67@gmail.com>
2026-02-11 22:57:41 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const YTMusic = require("ytmusic-api");
+const YTMusic = require("ytmusic-api").default;
 
 const app = express();
 app.use(cors());
