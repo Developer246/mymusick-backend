f5e810f50f3b731c2f02ef2a3f63e75a5552438a
Developer246 <bobicraftyt67@gmail.com>
2026-02-11 20:53:31 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const YTMusic = require("ytmusic-api");
+
+const app = express();
+app.use(cors());
+
+const ytmusic = new YTMusic();
+
+(async () => {
+  await ytmusic.initialize();
+})();
+
+app.get("/search", async (req, res) => {
+  const q = req.query.q;
+  if (!q) return res.json([]);
+
+  try {
+    const results = await ytmusic.search(q, "song");
+    res.json(results.slice(0, 5));
+  } catch (err) {
+    res.status(500).json({ error: "YT Music error" });
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor activo");
+});
