a1dba1542ae77206c2d15c1335167e997d86ee62
Developer246 <bobicraftyt67@gmail.com>
2026-03-02 21:24:24 -0600

    Actualizar server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const { Innertube } = require("youtubei.js");
-const fetch = require("node-fetch"); // asegúrate de tenerlo instalado
 
 const app = express();
 app.use(cors());
